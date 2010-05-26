883707263e729dd43c45e3c98dd126c25fd4ff68
Justin Etheredge <justin@etheredge.us>
2010-05-26 10:30:11 -0400

    Moving to version 0.5.0

---
+++ SquishIt.Framework/JavaScript/Minifiers/YuiMinifier.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Yahoo.Yui.Compressor;
+
+namespace SquishIt.Framework.JavaScript.Minifiers
+{
+    public class YuiMinifier: IJavaScriptCompressor
+    {
+        string IJavaScriptCompressor.Identifier
+        {
+            get { return Identifier; }
+        }
+        
+        public static string Identifier
+        {
+            get { return "YuiJavaScriptCompressor"; }
+        }
+
+        public string CompressContent(string content)
+        {
+            return JavaScriptCompressor.Compress(content);
+        }
+    }
+}
