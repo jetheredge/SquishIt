93f7d219d15b35f59b77033a6d77888fc9626a5d
C-J Berg <berg.cj@gmail.com>
2010-07-18 18:53:21 +0800

    Fixed DirectoryEnumerator's reading of ordering.txt, and added a test for it. Rewrote some code as LINQ-statements, and added at test for skipping of "-vsdoc.js" files. Minor code improvements of both DirectoryEnumerator and its tests.

--- SquishIt.Tests/Directories/DirectoryEnumeratorTests.cs
+++ SquishIt.Tests/Directories/DirectoryEnumeratorTests.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using NUnit.Framework;
@@ -12,23 +13,103 @@ namespace SquishIt.Framework.Tests
         [Test]
         public void CanEnumerateDirectory()
         {
-            var temporaryDirectory = Path.GetTempPath() + "\\" + Path.GetRandomFileName() + "\\";
-            System.IO.Directory.CreateDirectory(temporaryDirectory);
+            var temporaryDirectory = CreateNewTemporaryDirectory();
             try
             {
                 for (int i = 0; i < 3; i++)
                 {
-                    File.CreateText(temporaryDirectory + Path.GetRandomFileName() + ".js").Dispose();
+                    CreateJsFileWithRandomName(temporaryDirectory);
                 }                    
 
                 var directoryEnumerator = new DirectoryEnumerator();
-                var temporaryFiles = directoryEnumerator.GetFiles(temporaryDirectory).ToList();
-                Assert.AreEqual(3, temporaryFiles.Count);
+                var result = directoryEnumerator.GetFiles(temporaryDirectory).ToList();
+
+                Assert.AreEqual(3, result.Count);
             }
             finally
             {
                 System.IO.Directory.Delete(temporaryDirectory, true);
             }
+        }
+
+        [Test]
+        public void EnumerationSkipsVsDocFiles()
+        {
+            var temporaryDirectory = CreateNewTemporaryDirectory();
+            try
+            {
+                CreateJsFileWithRandomName(temporaryDirectory);
+                CreateJsFileWithRandomNameAndSuffix(temporaryDirectory, "-vsdoc");
+
+                var directoryEnumerator = new DirectoryEnumerator();
+                var result = directoryEnumerator.GetFiles(temporaryDirectory).ToList();
+
+                Assert.AreEqual(1, result.Count);
+            }
+            finally
+            {
+                System.IO.Directory.Delete(temporaryDirectory, true);
+            }
+        }
+
+        [Test]
+        public void CanOrderFilesInDirectory()
+        {
+            var temporaryDirectory = CreateNewTemporaryDirectory();
+            try
+            {
+                for (int i = 0; i < 4; i++)
+                {
+                    string fileName = String.Format("{0}File{1}.js", temporaryDirectory, (1 + i));
+                    File.CreateText(fileName).Dispose();
+                }
+                using (var ordering = File.CreateText(temporaryDirectory + "ordering.txt"))
+                {
+                    ordering.WriteLine("File3.js");
+                    ordering.WriteLine("File4.js");
+                    ordering.WriteLine("File1.js");
+                    ordering.WriteLine("File2.js");
+                }
+
+                var directoryEnumerator = new DirectoryEnumerator();
+                var result = directoryEnumerator.GetFiles(temporaryDirectory).ToList();
+
+                var expectedList = new List<string>
+                {
+                    temporaryDirectory + "File3.js",
+                    temporaryDirectory + "File4.js",
+                    temporaryDirectory + "File1.js",
+                    temporaryDirectory + "File2.js"
+                };
+                CollectionAssert.AreEqual(expectedList, result, StringComparer.OrdinalIgnoreCase);
+            }
+            finally
+            {
+                System.IO.Directory.Delete(temporaryDirectory, true);
+            }
+        }
+
+        private static string CreateNewTemporaryDirectory()
+        {
+            string temporaryDirectory = Path.GetTempPath() + "\\" + Path.GetRandomFileName() + "\\";
+            System.IO.Directory.CreateDirectory(temporaryDirectory);
+            return temporaryDirectory;
+        }
+
+        private static void CreateJsFileWithRandomName(string temporaryDirectory)
+        {
+            CreateJsFileWithRandomNameAndSuffix(temporaryDirectory, String.Empty);
+        }
+
+        private static void CreateJsFileWithRandomNameAndSuffix(string temporaryDirectory, string suffix)
+        {
+            CreateJsFile(temporaryDirectory, Path.GetRandomFileName() + suffix);
+        }
+
+        private static void CreateJsFile(string temporaryDirectory, string nameWithoutExtension)
+        {
+            string filePath = temporaryDirectory + nameWithoutExtension + ".js";
+            File.CreateText(filePath).Dispose();
         }
     }
 }
