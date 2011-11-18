4479af0897208823b2a3f3176c3be32b85adfe7d
Alex Ullrich <alex.ullrich@gmail.com>
2011-11-17 22:54:31 -0500

    being ridiculous about formatting

--- SquishIt.Tests/Stubs/PreprocessorScope.cs
+++ SquishIt.Tests/Stubs/PreprocessorScope.cs
@@ -3,7 +3,7 @@ using SquishIt.Framework;
 
 namespace SquishIt.Tests.Stubs 
 {
-    public class PreprocessorScope<T> : IDisposable where T:IPreprocessor {
+    public class PreprocessorScope<T> : IDisposable where T : IPreprocessor {
         public PreprocessorScope()
         {
             Bundle.RegisterPreprocessor<T>();
