82b4a3798a85376f27cf7ba86dc9c63112f4d457
Rodrigo Dumont <rlsdumont@gmail.com>
2011-11-19 01:49:36 -0200

    Added content files to preprocessor packages so they are automatically registered after install

---
+++ SquishIt.Less/Content/App_Start/SquishItLess.cs.pp
@@ -0,0 +1,15 @@
+﻿[assembly: WebActivator.PreApplicationStartMethod(typeof($rootnamespace$.App_Start.SquishItLess), "Start")]
+
+namespace $rootnamespace$.App_Start
+{
+    using SquishIt.Framework;
+    using SquishIt.Less;
+
+    public class SquishItLess
+    {
+        public static void Start()
+        {
+            Bundle.RegisterPreprocessor<LessPreprocessor>();
+        }
+    }
+}
