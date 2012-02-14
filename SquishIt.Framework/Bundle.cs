238c71e4bb72752f13f80ed229f4f31909b712d5
Alex Ullrich <alex.ullrich@gmail.com>
2012-02-13 21:15:24 -0500

    storing only a single list of preprocessors (still separate lists of allowed extensions)

--- SquishIt.Framework/Bundle.cs
+++ SquishIt.Framework/Bundle.cs
@@ -8,18 +8,11 @@ namespace SquishIt.Framework
 {
     public class Bundle
     {
-        internal static readonly List<IPreprocessor> GlobalPreprocessors = new List<IPreprocessor>();
-        internal static readonly List<IPreprocessor> ScriptPreprocessors = new List<IPreprocessor>();
-        internal static readonly List<IPreprocessor> StylePreprocessors = new List<IPreprocessor>();
+        internal static readonly List<IPreprocessor> Preprocessors = new List<IPreprocessor>();
 
         internal static readonly HashSet<String> AllowedGlobalExtensions = new HashSet<string>();
         internal static readonly HashSet<String> AllowedScriptExtensions = new HashSet<string> { ".JS" };
         internal static readonly HashSet<String> AllowedStyleExtensions = new HashSet<string> { ".CSS" };
-
-        internal static IEnumerable<IPreprocessor> Preprocessors
-        {
-            get { return GlobalPreprocessors.Union(ScriptPreprocessors).Union(StylePreprocessors); }
-        }
 
         //TODO: provide a way to globally register preprocessor, registering extension w/ both JS and CSS allowed extensions?
         public static void RegisterGlobalPreprocessor<T>(T instance) where T : IPreprocessor
@@ -29,7 +22,7 @@ namespace SquishIt.Framework
                 throw new InvalidOperationException(string.Format("Can't add multiple preprocessors of type {0}", typeof(T).FullName));
             }
             foreach (var ext in instance.Extensions) AllowedGlobalExtensions.Add(ext.ToUpper());
-            GlobalPreprocessors.Add(instance);
+            Preprocessors.Add(instance);
         }
 
         public static void RegisterScriptPreprocessor<T>(T instance) where T : IPreprocessor 
@@ -39,7 +32,7 @@ namespace SquishIt.Framework
                 throw new InvalidOperationException(string.Format("Can't add multiple preprocessors of type {0}", typeof(T).FullName));
             }
             foreach (var ext in instance.Extensions) AllowedScriptExtensions.Add(ext.ToUpper());
-            ScriptPreprocessors.Add(instance);
+            Preprocessors.Add(instance);
         }
 
         public static void RegisterStylePreprocessor<T>(T instance) where T : IPreprocessor 
@@ -49,14 +42,12 @@ namespace SquishIt.Framework
                 throw new InvalidOperationException(string.Format("Can't add multiple preprocessors of type {0}", typeof(T).FullName));
             }
             foreach (var ext in instance.Extensions) AllowedStyleExtensions.Add(ext.ToUpper());
-            StylePreprocessors.Add(instance);
+            Preprocessors.Add(instance);
         }
 
         public static void ClearPreprocessors()
         {
-            GlobalPreprocessors.Clear();
-            ScriptPreprocessors.Clear();
-            StylePreprocessors.Clear();
+            Preprocessors.Clear();
 
             AllowedGlobalExtensions.Clear();
             AllowedScriptExtensions.Clear();
