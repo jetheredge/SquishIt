4ba0f4532ab35d38b1b37f80a0dd9414abafcd9e
Alex Ullrich <alex.ullrich@gmail.com>
2012-05-18 10:51:14 -0400

    Don't check machine configuration to determine if debugging is enabled in low-trust environments (fix #187) Add tests for DebugStatusReader

--- SquishIt.Framework/Utilities/DebugStatusReader.cs
+++ SquishIt.Framework/Utilities/DebugStatusReader.cs
@@ -1,42 +1,41 @@
 using System;
-using System.Configuration;
 using System.Web;
-using System.Web.Configuration;
 
 namespace SquishIt.Framework.Utilities
 {
-    public class DebugStatusReader: IDebugStatusReader
+
+    public class DebugStatusReader : IDebugStatusReader
     {
+        readonly IMachineConfigReader machineConfigReader;
         private bool forceDebug = false;
         private bool forceRelease = false;
 
+        public DebugStatusReader()
+            : this(new MachineConfigReader())
+        {
+
+        }
+
+        internal DebugStatusReader(IMachineConfigReader machineConfigReader)
+        {
+            this.machineConfigReader = machineConfigReader;
+        }
+
         public bool IsDebuggingEnabled()
         {
-            if (forceDebug)
+            if(forceDebug)
             {
                 return true;
             }
 
-            if (forceRelease)
+            if(forceRelease)
             {
                 return false;
             }
-            
-            if (HttpContext.Current != null && HttpContext.Current.IsDebuggingEnabled)
-            {                
-                //check retail setting in machine.config
-                //Thanks Dave Ward! http://www.encosia.com
-                System.Configuration.Configuration machineConfig = ConfigurationManager.OpenMachineConfiguration();
-                var group = machineConfig.GetSectionGroup("system.web");
-                if (group != null)
-                {
-                    var appSettingSection = (DeploymentSection)group.Sections["deployment"];
-                    if (appSettingSection.Retail)
-                    {
-                        return false;
-                    }
-                }                
-                return true;
+
+            if(HttpContext.Current != null && HttpContext.Current.IsDebuggingEnabled)
+            {
+                return !TrustLevel.IsHighOrUnrestrictedTrust || machineConfigReader.IsRetailDeployment;
             }
             return false;
         }
