da3cdf47e815ec7b0a6f11f7c655772d06fc4238
Alex Ullrich <alex.ullrich@gmail.com>
2012-07-02 19:40:10 -0400

    add test for loading hogan template from file system

--- SquishIt.Tests/HoganTests.cs
+++ SquishIt.Tests/HoganTests.cs
@@ -1,7 +1,9 @@
 ﻿using System.Text;
 using NUnit.Framework;
+using SquishIt.Framework.Resolvers;
 using SquishIt.Hogan;
 using SquishIt.Tests.Helpers;
+using SquishIt.Tests.Stubs;
 
 namespace SquishIt.Tests
 {
@@ -17,15 +19,51 @@ namespace SquishIt.Tests
         }
 
         [Test]
+        public void CanBundleJavascriptInDebug()
+        {
+            const string template = "<h1>{{message}}</h1>";
+            var templateFileName = "test.html";
+            var resolver = StubResolver.ForFile(templateFileName);
+
+            var readerFactory = new StubFileReaderFactory();
+            readerFactory.SetContents(template);
+
+            var writerFactory = new StubFileWriterFactory();
+
+            string tag;
+
+            using(new ResolverFactoryScope(typeof(FileSystemResolver).FullName, resolver))
+            {
+                tag = javaScriptBundleFactory
+                    .WithFileReaderFactory(readerFactory)
+                    .WithFileWriterFactory(writerFactory)
+                    .WithDebuggingEnabled(true)
+                    .Create()
+                    .WithPreprocessor(new HoganPreprocessor())
+                    .Add(templateFileName)
+                    .Render("~/template.js");
+            }
+
+            var sb = new StringBuilder();
+            sb.Append(@"var JST = JST || {};");
+            sb.Append(@"JST['dummy'] = new Hogan.Template(function(c,p,i){var _=this;_.b(i=i||"""");_.b(""<h1>"");_.b(_.v(_.f(""message"",c,p,0)));_.b(""</h1>"");return _.fl();;});");
+            Assert.AreEqual(1, writerFactory.Files.Count);
+            var expectedTag = "<script type=\"text/javascript\" src=\"test.html.squishit.debug.js\"></script>\n";
+            Assert.AreEqual(expectedTag, TestUtilities.NormalizeLineEndings(tag));
+
+            Assert.AreEqual(sb.ToString(), writerFactory.Files[TestUtilities.PrepareRelativePath("test.html.debug.jstest.html")]);
+        }
+
+        [Test]
         public void CanBundleJavascriptInDebugWithArbitraryHogan()
         {
-            const string tempalte = "<h1>{{message}}</h1>";
+            const string template = "<h1>{{message}}</h1>";
 
             var tag = javaScriptBundleFactory
                 .WithDebuggingEnabled(true)
                 .Create()
                 .WithPreprocessor(new HoganPreprocessor())
-                .AddString(tempalte, ".html")
+                .AddString(template, ".html")
                 .Render("~/template.js");
             
             var sb = new StringBuilder();
