b7485b1da7a0a4ea081d8467bdd1b431c72521fb
jincod <vadim.abdrashitov@gmail.com>
2012-07-02 16:26:09 +0600

    rename some files

--- SquishIt.Tests/Helpers/CssBundleFactory.cs
+++ SquishIt.Tests/Helpers/CssBundleFactory.cs
@@ -1,12 +1,12 @@
 using SquishIt.Framework;
-using SquishIt.Framework.JavaScript;
+using SquishIt.Framework.Css;
 using SquishIt.Framework.Files;
 using SquishIt.Framework.Utilities;
 using SquishIt.Tests.Stubs;
 
 namespace SquishIt.Tests.Helpers
 {
-    internal class JavaScriptBundleFactory
+    internal class CssBundleFactory
     {
         IDebugStatusReader debugStatusReader = new StubDebugStatusReader();
         IFileWriterFactory fileWriterFactory = new StubFileWriterFactory();
@@ -18,42 +18,42 @@ namespace SquishIt.Tests.Helpers
         public StubFileReaderFactory FileReaderFactory { get { return fileReaderFactory as StubFileReaderFactory; } }
         public StubFileWriterFactory FileWriterFactory { get { return fileWriterFactory as StubFileWriterFactory; } }
 
-        public JavaScriptBundleFactory WithDebuggingEnabled(bool enabled)
+        public CssBundleFactory WithDebuggingEnabled(bool enabled)
         {
             this.debugStatusReader = new StubDebugStatusReader(enabled);
             return this;
         }
 
-        public JavaScriptBundleFactory WithFileWriterFactory(IFileWriterFactory fileWriterFactory)
+        public CssBundleFactory WithFileWriterFactory(IFileWriterFactory fileWriterFactory)
         {
             this.fileWriterFactory = fileWriterFactory;
             return this;
         }
 
-        public JavaScriptBundleFactory WithFileReaderFactory(IFileReaderFactory fileReaderFactory)
+        public CssBundleFactory WithFileReaderFactory(IFileReaderFactory fileReaderFactory)
         {
             this.fileReaderFactory = fileReaderFactory;
             return this;
         }
 
-        JavaScriptBundleFactory WithCurrentDirectoryWrapper(ICurrentDirectoryWrapper currentDirectoryWrapper)
+        CssBundleFactory WithCurrentDirectoryWrapper(ICurrentDirectoryWrapper currentDirectoryWrapper)
         {
             this.currentDirectoryWrapper = currentDirectoryWrapper;
             return this;
         }
 
-        public JavaScriptBundleFactory WithHasher(IHasher hasher)
+        public CssBundleFactory WithHasher(IHasher hasher)
         {
             this.hasher = hasher;
             return this;
         }
 
-        public JavaScriptBundle Create()
+        public CSSBundle Create()
         {
-            return new JavaScriptBundle(debugStatusReader, fileWriterFactory, fileReaderFactory, currentDirectoryWrapper, hasher, bundleCache);
+            return new CSSBundle(debugStatusReader, fileWriterFactory, fileReaderFactory, currentDirectoryWrapper, hasher, bundleCache);
         }
 
-        public JavaScriptBundleFactory WithContents(string css)
+        public CssBundleFactory WithContents(string css)
         {
             (fileReaderFactory as StubFileReaderFactory).SetContents(css);
             return this;
