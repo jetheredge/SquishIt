0119fb597c146adb07a523393736265e5ef701b5
Alex Ullrich <alex.ullrich@gmail.com>
2012-07-02 20:11:31 -0400

    add hogan tests for release mode + templates from filesystem

--- SquishIt.Tests/HoganTests.cs
+++ SquishIt.Tests/HoganTests.cs
@@ -23,10 +23,10 @@ namespace SquishIt.Tests
         {
             const string template = "<h1>{{message}}</h1>";
             var templateFileName = "test.html";
-            var resolver = StubResolver.ForFile(templateFileName);
+            var resolver = StubResolver.ForFile(TestUtilities.PrepareRelativePath(templateFileName));
 
             var readerFactory = new StubFileReaderFactory();
-            readerFactory.SetContents(template);
+            readerFactory.SetContentsForFile(TestUtilities.PrepareRelativePath(templateFileName), template);
 
             var writerFactory = new StubFileWriterFactory();
 
@@ -40,18 +40,20 @@ namespace SquishIt.Tests
                     .WithDebuggingEnabled(true)
                     .Create()
                     .WithPreprocessor(new HoganPreprocessor())
-                    .Add(templateFileName)
+                    .Add("~/" + templateFileName)
                     .Render("~/template.js");
             }
 
             var sb = new StringBuilder();
-            sb.Append(@"var JST = JST || {};");
-            sb.Append(@"JST['dummy'] = new Hogan.Template(function(c,p,i){var _=this;_.b(i=i||"""");_.b(""<h1>"");_.b(_.v(_.f(""message"",c,p,0)));_.b(""</h1>"");return _.fl();;});");
+            sb.AppendLine(@"var JST = JST || {};");
+            sb.AppendLine(@"JST['test'] = new Hogan.Template(function(c,p,i){var _=this;_.b(i=i||"""");_.b(""<h1>"");_.b(_.v(_.f(""message"",c,p,0)));_.b(""</h1>"");return _.fl();;});");
+            var compiled = sb.ToString();
+
             Assert.AreEqual(1, writerFactory.Files.Count);
             var expectedTag = "<script type=\"text/javascript\" src=\"test.html.squishit.debug.js\"></script>\n";
             Assert.AreEqual(expectedTag, TestUtilities.NormalizeLineEndings(tag));
 
-            Assert.AreEqual(sb.ToString(), writerFactory.Files[TestUtilities.PrepareRelativePath("test.html.debug.jstest.html")]);
+            Assert.AreEqual(compiled, writerFactory.Files[TestUtilities.PrepareRelativePath("test.html.squishit.debug.js")]);
         }
 
         [Test]
@@ -65,12 +67,79 @@ namespace SquishIt.Tests
                 .WithPreprocessor(new HoganPreprocessor())
                 .AddString(template, ".html")
                 .Render("~/template.js");
-            
+
             var sb = new StringBuilder();
             sb.AppendLine(@"<script type=""text/javascript"">var JST = JST || {};");
             sb.AppendLine(@"JST['dummy'] = new Hogan.Template(function(c,p,i){var _=this;_.b(i=i||"""");_.b(""<h1>"");_.b(_.v(_.f(""message"",c,p,0)));_.b(""</h1>"");return _.fl();;});");
             sb.AppendLine("</script>");
-            Assert.AreEqual(sb.ToString(),tag);
+            Assert.AreEqual(sb.ToString(), tag);
+        }
+
+        [Test]
+        public void CanBundleJavascriptInRelease()
+        {
+            const string template = "<h1>{{message}}</h1>";
+            var templateFileName = "test.html";
+            var resolver = StubResolver.ForFile(TestUtilities.PrepareRelativePath(templateFileName));
+
+            var readerFactory = new StubFileReaderFactory();
+            readerFactory.SetContentsForFile(TestUtilities.PrepareRelativePath(templateFileName), template);
+
+            var writerFactory = new StubFileWriterFactory();
+
+            string tag;
+
+            using(new ResolverFactoryScope(typeof(FileSystemResolver).FullName, resolver))
+            {
+                tag = javaScriptBundleFactory
+                    .WithFileReaderFactory(readerFactory)
+                    .WithFileWriterFactory(writerFactory)
+                    .WithDebuggingEnabled(false)
+                    .Create()
+                    .WithPreprocessor(new HoganPreprocessor())
+                    .Add("~/" + templateFileName)
+                    .Render("~/template.js");
+            }
+
+            //are minifier's optimizations here OK?
+            var compiled =
+                @"var JST=JST||{};JST.test=new Hogan.Template(function(n,t,i){var r=this;return r.b(i=i||""""),r.b(""<h1>""),r.b(r.v(r.f(""message"",n,t,0))),r.b(""</h1>""),r.fl()})";
+
+            Assert.AreEqual(1, writerFactory.Files.Count);
+            var expectedTag = "<script type=\"text/javascript\" src=\"template.js?r=hash\"></script>";
+            Assert.AreEqual(expectedTag, TestUtilities.NormalizeLineEndings(tag));
+
+            var actual = writerFactory.Files[TestUtilities.PrepareRelativePath("template.js")];
+            Assert.AreEqual(compiled, actual);
+        }
+
+        [Test]
+        public void CanBundleJavascriptInReleaseWithArbitraryHogan()
+        {
+            const string template = "<h1>{{message}}</h1>";
+
+            var writerFactory = new StubFileWriterFactory();
+
+            string tag;
+
+            tag = javaScriptBundleFactory
+                    .WithFileWriterFactory(writerFactory)
+                    .WithDebuggingEnabled(false)
+                    .Create()
+                    .WithPreprocessor(new HoganPreprocessor())
+                    .AddString(template, ".html")
+                    .Render("~/template.js");
+
+            //are minifier's optimizations here OK?
+            var compiled =
+                @"var JST=JST||{};JST[""dummy.""]=new Hogan.Template(function(n,t,i){var r=this;return r.b(i=i||""""),r.b(""<h1>""),r.b(r.v(r.f(""message"",n,t,0))),r.b(""</h1>""),r.fl()})";
+
+            Assert.AreEqual(1, writerFactory.Files.Count);
+            var expectedTag = "<script type=\"text/javascript\" src=\"template.js?r=hash\"></script>";
+            Assert.AreEqual(expectedTag, TestUtilities.NormalizeLineEndings(tag));
+
+            var actual = writerFactory.Files[TestUtilities.PrepareRelativePath("template.js")];
+            Assert.AreEqual(compiled, actual);
         }
     }
 }
