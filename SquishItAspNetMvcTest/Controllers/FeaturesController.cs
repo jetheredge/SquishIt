1943045018b700e345cabf308c8d0fdd59ae4735
Justin Etheredge <justin@etheredge.us>
2012-07-05 09:51:00 -0400

    Adding tests for css and js compression.

---
+++ SquishItAspNetMvcTest/Controllers/FeaturesController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SquishItAspNetMvcTest.Controllers
+{
+    public class FeaturesController : Controller
+    {
+        public ActionResult MinifyCss()
+        {
+            return View();
+        }
+
+        public ActionResult MinifyJs()
+        {
+            return View();
+        }
+    }
+}
---
+++ SquishItAspNetMvcTest/assets/js/minifyjs_test.js
@@ -0,0 +1,3 @@
+﻿$(function () {
+    $("#js-test-container").html("This text was placed here by javascript. If you see this, the test completed successfully.");
+});
