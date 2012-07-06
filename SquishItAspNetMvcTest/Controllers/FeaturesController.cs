554bff9011c88a07fd8c471e2f76db479cbc5542
Justin Etheredge <justin@etheredge.us>
2012-07-06 09:30:29 -0400

    Adding tests for css imports.

--- SquishItAspNetMvcTest/Controllers/FeaturesController.cs
+++ SquishItAspNetMvcTest/Controllers/FeaturesController.cs
@@ -22,5 +22,10 @@ namespace SquishItAspNetMvcTest.Controllers
         {
             return View();
         }
+
+        public ActionResult Imports()
+        {
+            return View();
+        }
     }
 }
