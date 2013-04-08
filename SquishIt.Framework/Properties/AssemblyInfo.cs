8edb9ca879975814fbdef51454267d2c81a9b2bf
Alex Ullrich <alex.ullrich@gmail.com>
2013-04-08 15:43:54 -0400

    update AssemblyInfo version to 0.9.3

--- SquishIt.Framework/Properties/AssemblyInfo.cs
+++ SquishIt.Framework/Properties/AssemblyInfo.cs
@@ -36,7 +36,7 @@ using System.Security;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.9.0.0")]
-[assembly: AssemblyFileVersion("0.9.0.0")]
+[assembly: AssemblyVersion("0.9.3.0")]
+[assembly: AssemblyFileVersion("0.9.3.0")]
 [assembly: InternalsVisibleTo("SquishIt.Tests")]
 [assembly: AllowPartiallyTrustedCallers]
