e4494a504f3200169507c46eb2ce15d2346e9abd
Alex Ullrich <alex.ullrich@gmail.com>
2014-03-09 20:45:15 -0400

    add nuspec and attempt to add .cs.pp file for NSass preprocessor

---
+++ SquishIt.NSass/Content/App_Start/SquishItNSass.cs.pp
@@ -0,0 +1,15 @@
+﻿[assembly: WebActivator.PreApplicationStartMethod(typeof($rootnamespace$.App_Start.SquishItNSass), "Start")]
+
+namespace $rootnamespace$.App_Start
+{
+    using SquishIt.Framework;
+    using SquishIt.NSass;
+
+    public class SquishItNSass
+    {
+        public static void Start()
+        {
+            Bundle.RegisterStylePreprocessor(new NSassPreprocessor());
+        }
+    }
+}
