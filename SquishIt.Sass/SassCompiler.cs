f08f31b72c8828ac87677e63b691fd6302046a2b
Alex Ullrich <alex.ullrich@gmail.com>
2014-06-06 08:40:22 -0400

    replace ironruby / sass-in-one with libsass-net

--- SquishIt.Sass/SassCompiler.cs
+++ SquishIt.Sass/SassCompiler.cs
@@ -1,31 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Reflection;
-using System.Text.RegularExpressions;
-using IronRuby;
-using Microsoft.Scripting;
-using Microsoft.Scripting.Hosting;
-using SquishIt.Framework;
+﻿using System.Text.RegularExpressions;
+using LibSassNet;
 
 namespace SquishIt.Sass
 {
-    //much thanks here to Paul Betts' SassAndCoffee project (https://github.com/xpaulbettsx/SassAndCoffee)
     public class SassCompiler
     {
+        private readonly ISassCompiler _compiler = new LibSassNet.SassCompiler();
+
         internal static string RootAppPath;
 
         public SassCompiler(string rootPath)
         {
             RootAppPath = rootPath;
-        }
-
-        public string CompileSass(string input, string fileName)
-        {
-            SassMode sassMode = SassMode.Scss;
-            if(Regex.IsMatch(fileName, @"\.sass$")) sassMode = SassMode.Sass;
-
-            return CompileSass(input, sassMode);
         }
 
         public enum SassMode
@@ -36,75 +22,13 @@ namespace SquishIt.Sass
 
         public string CompileSass(string input, SassMode mode)
         {
-            var srs = new ScriptRuntimeSetup() { HostType = typeof(ResourceAwareScriptHost) };
-            srs.AddRubySetup();
-            var runtime = Ruby.CreateRuntime(srs);
-
-            var engine = runtime.GetRubyEngine();
-
-            engine.SetSearchPaths(new List<string> { @"R:\lib\ironruby", @"R:\lib\ruby\1.9.1" });
-
-            var source = engine.CreateScriptSourceFromString(Utility.ResourceAsString("SquishIt.Sass.lib.sass_in_one.rb"), SourceCodeKind.File);
-            var scope = engine.CreateScope();
-            source.Execute(scope);
-
-            dynamic sassMode = mode == SassMode.Sass
-                                   ? engine.Execute("{:syntax => :sass}")
-                                   : engine.Execute("{:syntax => :scss}");
-
-            var sassEngine = scope.Engine.Runtime.Globals.GetVariable("Sass");
-
-            return (string) sassEngine.compile(input, sassMode);
-        }
-    }
-
-    public class ResourceAwareScriptHost : ScriptHost
-    {
-        PlatformAdaptationLayer _innerPal = null;
-        public override PlatformAdaptationLayer PlatformAdaptationLayer
-        {
-            get { return _innerPal ?? (_innerPal = new ResourceAwarePAL()); }
-        }
-    }
-
-    public class ResourceAwarePAL : PlatformAdaptationLayer
-    {
-        public override Stream OpenInputFileStream(string path)
-        {
-            var ret = Assembly.GetExecutingAssembly().GetManifestResourceStream(pathToResourceName(path));
-            if (ret != null)
-            {
-                return ret;
-            }
-
-            if (SassCompiler.RootAppPath == null || !path.ToLowerInvariant().StartsWith(SassCompiler.RootAppPath))
-            {
-                return null;
-            }
-
-            return base.OpenInputFileStream(path);
+            var processedInput = mode == SassMode.Scss ? input : ConvertToScss(input);
+            return _compiler.Compile(processedInput, OutputStyle.Nested, SourceCommentsMode.None, null);
         }
 
-        public override bool FileExists(string path)
+        internal string ConvertToScss(string input)
         {
-            if (Assembly.GetExecutingAssembly().GetManifestResourceInfo(pathToResourceName(path)) != null)
-            {
-                return true;
-            }
-
-            return base.FileExists(path);
-        }
-
-        string pathToResourceName(string path)
-        {
-            path = Platform.Mono ?
-                path.Replace(Environment.CurrentDirectory, string.Empty).TrimStart(new [] { '/' }):
-                path.Replace("1.9.1", "_1._9._1");
-            
-            return path   
-                .Replace('\\', '.')
-                .Replace('/', '.')
-                .Replace("R:", "SquishIt.Sass");
+            throw new System.NotImplementedException("need to convert Sass to Scss, see http://sasstoscss.com/ maybe?");
         }
     }
 }
