aa48d387fd1fa89a369cf589d21713f209df8695
Alex Ullrich <alex.ullrich@gmail.com>
2014-12-24 08:26:09 -0500

    prep for 0.9.7 release

--- SquishIt.Framework/Properties/AssemblyInfo.cs
+++ SquishIt.Framework/Properties/AssemblyInfo.cs
@@ -39,7 +39,7 @@ using System.Security;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.9.6.1")]
-[assembly: AssemblyFileVersion("0.9.6.1")]
+[assembly: AssemblyVersion("0.9.7")]
+[assembly: AssemblyFileVersion("0.9.7")]
 [assembly: InternalsVisibleTo("SquishIt.Tests")]
 [assembly: AllowPartiallyTrustedCallers]
