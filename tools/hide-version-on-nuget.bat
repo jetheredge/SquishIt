adc4964785013135bd79591cb64a833c0bc4c3bc
Alex Ullrich <alex.ullrich@gmail.com>
2016-12-11 18:29:20 -0500

    include Source parameter in nuget scripts, pass vs version to build

--- tools/hide-version-on-nuget.bat
+++ tools/hide-version-on-nuget.bat
@@ -1,3 +1,3 @@
 set packages=(SquishIt SquishIt.CoffeeScript SquishIt.Hogan SquishIt.Less SquishIt.MsIeCoffeeScript SquishIt.MsIeHogan SquishIt.Mvc SquishIt.NSass SquishIt.Sass)
 
-for %%p in %packages% do nuget delete %%p %1 -NoPrompt
+for %%p in %packages% do nuget delete %%p %1 -NoPrompt -Source https://www.nuget.org/api/v2/package
--- tools/pack-push.bat
+++ tools/pack-push.bat
@@ -1,8 +1,8 @@
-msbuild ..\SquishIt.sln /p:Configuration=Release /p:Platform="Any CPU"
+msbuild ..\SquishIt.sln /p:Configuration=Release /p:Platform="Any CPU" /p:VisualStudioVersion=14.0
 for %%s in (..\nuspec\*.nuspec) do (
 	nuget pack %%s
 )
 for %%p in (*.nupkg) do (
-	nuget push %%p
+	nuget push %%p -Source https://www.nuget.org/api/v2/package
 	del %%p
 )
